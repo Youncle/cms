a31a8dfee3a721000817fe15cceadf9802d7ac95
Youncle <carstocea_vladimir@yahoo.com>
2018-04-05 12:05:56 +0300

    php cms

---
+++ admin/js/scripts.js
@@ -0,0 +1,43 @@
+$(document).ready(function(){
+
+$('#selectAllBoxes').click(function(event){
+
+    if (this.checked) {
+
+        $('.checkBoxes').each(function(){
+
+            this.checked=true;
+        })
+    } else
+    {
+        this.checked=false;
+    }
+
+});
+
+var div_box="<div id='load-screen'><div id='loading'></div></div>";
+
+$("body").prepend(div_box);
+
+$('#load-screen').delay(300).fadeOut(600, function(){
+    $(this).remove();
+});
+
+
+});
+
+function loadUsersOnline()
+{
+    $.get("functions.php?onlineusers=result", function(data){
+
+        $(".useronline").text(data);
+    });
+}
+
+setInterval(function(){
+
+    loadUsersOnline();
+
+},500);
+
+
